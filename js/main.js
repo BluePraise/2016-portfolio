14005d3e7952ec6d9ff8026fb420cb2490a43461
Magalie Linda <hellothere@magalielinda.me>
2016-02-23 14:16:20 +0100

    contact label now animated except for going back

--- js/main.js
+++ js/main.js
@@ -12,38 +12,53 @@
 // } )( jQuery );
 
 //Back to top
-jQuery(document).ready(function($){
-  // browser window scroll (in pixels) after which the "back to top" link is shown
-  var offset = 300,
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-    offset_opacity = 1200,
-    //duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700,
-    //grab the "back to top" link
-    $back_to_top = $('.cd-top');
+jQuery(document).ready(function($) {
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
 
-  //hide or show the "back to top" link
-  $(window).scroll(function(){
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-    if( $(this).scrollTop() > offset_opacity ) { 
-      $back_to_top.addClass('cd-fade-out');
-    }
-  });
+    //hide or show the "back to top" link
+    $(window).scroll(function() {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
 
-  //smooth scroll to top
-  $back_to_top.on('click', function(event){
-    event.preventDefault();
-    $('body,html').animate({
-      scrollTop: 0 ,
-      }, scroll_top_duration
-    );
-  });
+    //smooth scroll to top
+    $back_to_top.on('click', function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
 
 
 });
 
-$('document').ready(function(){
-  $('.first').delay('700').fadeTo('700', 1);
-  $('.second').delay('1000').fadeTo('700', 1);
+$('document').ready(function() {
+    $('.first').delay('700').fadeTo('700', 1);
+    $('.second').delay('1000').fadeTo('700', 1);
+    $('.talk-to-me').delay('1300').fadeTo('700', 1);
+});
 
-}); 
+$('.talk-to-me').on('click', function(e) {
+  e.preventDefault();
+  var cl = $('.contact-list');
+  cl.addClass('show-this');
+  $('.show-this').animate({left: "-4px"});
+  $('.talk-to-me').addClass('go-away');
+});
+
+$('.go-away').on('click', function(event) {
+  console.log("default " + event.type + " prevented");
+  alert("cynthia sucks");
+  event.preventDefault();
+  // $('.show-this').animate({left: "-220px"});
+  // $('.talk-to-me').removeClass('go-away');
+});
