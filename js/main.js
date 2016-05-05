66edf7c1876638d96178ffd657305aa4b8a2928a
Magalie Linda <hellothere@magalielinda.me>
2016-05-05 13:06:10 +0200

    hero cover

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-//Parallaxing
 
-
-// ( function( $ ) {
-//     // Init Skrollr
-//     var s = skrollr.init({
-//         render: function(data) {
-//             //Debugging - Log the current scroll position.
-//             //console.log(data.curTop);
-//         }
-//     });
-// } )( jQuery );
 
 //Back to top
 jQuery(document).ready(function($) {
@@ -43,6 +32,7 @@ jQuery(document).ready(function($) {
 $('document').ready(function() {
     $('.first').delay('700').fadeTo('700', 1);
     $('.second').delay('1000').fadeTo('700', 1);
+    $('.third').delay('1300').fadeTo('700', 1);
     $('.talk-to-me').delay('1300').fadeTo('700', 1);
 
     $('.show').on('click', function(event) {
